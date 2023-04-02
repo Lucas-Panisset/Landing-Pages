4f96f37695c20bd4b2bd551a0d5d3eb9213c12a3
Panisset <lucas.panisset1@gmail.com>
2023-04-02 13:50:32 -0300

    Adicionar arquivos de projeto.

---
+++ automatização/teste.py
@@ -0,0 +1,10 @@
+from multiprocessing.connection import wait
+from time import sleep
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+import websocket
+import json
